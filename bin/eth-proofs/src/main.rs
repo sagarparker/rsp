7185ed724368b521fbe93cd2bbace78f15023b3b
sagarparker <sagar8parker@gmail.com>
2025-08-11 11:17:45 +0000

    Fix for main.rs

--- bin/eth-proofs/src/main.rs
+++ bin/eth-proofs/src/main.rs
@@ -3,7 +3,6 @@ use std::sync::Arc;
 use alloy_provider::{Provider, ProviderBuilder, WsConnect};
 use clap::Parser;
 use cli::Args;
-use diesel::prelude::*;
 use eth_proofs::EthProofsClient;
 use futures::StreamExt;
 use rsp_host_executor::{
@@ -17,14 +16,6 @@ use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Env
 mod cli;
 
 mod eth_proofs;
-
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
